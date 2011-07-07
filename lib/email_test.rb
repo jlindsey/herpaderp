e5c38ae5c9e57acc06de28fdcedb6de1d07e30a7
Josh Lindsey <josh@cloudspace.com>
2011-07-07 12:41:49 -0400

    Remove logging stuff from libs

--- lib/email_test.rb
+++ lib/email_test.rb
@@ -1,20 +1,8 @@
-require 'logger'
 
-module EmailTest
-  @@logger = nil
-  LogFile = File.expand_path('log/email_test.log', File.dirname(__FILE__))
-  LogLevel = Logger::INFO
-
-  def self.logger
-    if @@logger.nil?
-      require 'fileutils'
-      FileUtils.mkpath File.dirname(LogFile)
-
-      @@logger = Logger.new LogFile
-      @@logger.level = LogLevel
-    end
-
-    @@logger
-  end
-end
+##
+# Namespace module. Encapsulates all of the other cool stuff.
+#
+# @author Josh Lindsey
+#
+module EmailTest; end
 
