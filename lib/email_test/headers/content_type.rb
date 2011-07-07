bd85558423045828beafd3fd5f2dec2c7c8879b7
Josh Lindsey <joshua.s.lindsey@gmail.com>
2011-07-07 19:20:21 -0400

    Flesh out ContentType header class with specs

--- lib/email_test/headers/content_type.rb
+++ lib/email_test/headers/content_type.rb
@@ -6,7 +6,38 @@ module EmailTest
     #
     # @author Josh Lindsey
     class ContentType < Headers::Header
+      ## @return [Hash] The parsed attributes for this Content-Type
+      attr_reader :attributes
+      
+      ##
+      # Parses and returns a new ContentType encapsulation.
+      # 
+      # (see EmailTest::Headers::Header#initialize)
+      #
+      # @author Josh Lindsey
+      def initialize raw_header
+        super
 
+        @attributes = {}
+        parse_attributes!
+      end
+
+      private
+
+      ##
+      # Parses the attributes out of the {#value} and puts them in the {#attributes} hash.
+      #
+      # @author Josh Lindsey
+      def parse_attributes!
+        parts = @value.split(';')
+        @value = parts.shift.strip
+
+        parts.each do |part|
+          part.strip!
+          k,v = part.split('=')
+          @attributes[k] = v.gsub('"', '') # remove quotes from eg. boundary attr
+        end
+      end
     end
   end
 end
--- spec/content_type_header_spec.rb
+++ spec/content_type_header_spec.rb
@@ -1,8 +1,36 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe EmailTest::Headers::ContentType do
-  it "should extend the base Header class"
-  it "should parse a Content-Type with a boundary"
-  it "should parse a Content-Type with a charset"
+  before(:each) do
+    @content_type_str = %{Content-Type: multipart/alternative;
+     boundary="------------090807010703040106060608"}
+    @content_type_with_charset = %{Content-Type: text/plain; charset=ISO-8859-1; format=flowed}
+    @content_type = EmailTest::Headers::ContentType.new @content_type_str
+  end
+
+  it "extends the base Header class" do
+    @content_type.should be_a(EmailTest::Headers::Header)
+  end
+
+  it "allows access to attributes as a Hash" do
+    @content_type.should respond_to(:attributes)
+    @content_type.should_not respond_to(:attributes=)
+
+    @content_type.attributes.should be_a(Hash)
+  end
+
+  it "parses a Content-Type with a boundary" do
+    @content_type.key.should == "Content-Type"
+    @content_type.value.should == "multipart/alternative"
+    @content_type.attributes.should == { "boundary" => "------------090807010703040106060608" }
+  end
+
+  it "parses a Content-Type with a charset" do
+    @content_type = EmailTest::Headers::ContentType.new @content_type_with_charset
+
+    @content_type.key.should == "Content-Type"
+    @content_type.value.should == "text/plain"
+    @content_type.attributes.should == { "charset" => "ISO-8859-1", "format" => "flowed" }
+  end
 end
 
