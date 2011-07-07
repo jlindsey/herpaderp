6c6b1819691b1215d3e6156579418ef73d31a091
Josh Lindsey <joshua.s.lindsey@gmail.com>
2011-07-07 18:49:59 -0400

    Add initial ContentType header and spec

---
+++ lib/email_test/headers/content_type.rb
@@ -0,0 +1,13 @@
+module EmailTest
+  module Headers
+
+    ##
+    # Parses and encapsulates the Content-Type header.
+    #
+    # @author Josh Lindsey
+    class ContentType < Headers::Header
+
+    end
+  end
+end
+
---
+++ spec/content_type_header_spec.rb
@@ -0,0 +1,8 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe EmailTest::Headers::ContentType do
+  it "should extend the base Header class"
+  it "should parse a Content-Type with a boundary"
+  it "should parse a Content-Type with a charset"
+end
+
